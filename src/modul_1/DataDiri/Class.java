892640992ab7ec49df6531e838a732cd0c96eadb
MUHAMMAD RAYYAN ALFARISY <rayyanalfarisy@gmail.com>
2025-09-12 11:22:31 +0700

    Data Diri

---
+++ src/modul_1/DataDiri/Class.java
@@ -0,0 +1,25 @@
+package modul_1.DataDiri;
+
+import java.time.LocalDate;
+
+public class Class {
+    public static void main(String[]args){
+    String nama = "Muhammad Rayyan Alfarisy";
+    String nim = "2024573010118";
+    String alamat="Lhokseumawe";
+    String prodi="Teknik Informatika";
+    String jurusan="Teknologi Informasi dan Komputer";
+    LocalDate tanggalLahir=LocalDate.of(2005,12,26 );
+    int semester=3;
+    String kelas="TI 2A";
+
+    System.out.println(" Nama          : " + nama);
+    System.out.println(" Nim           : " + nim);
+    System.out.println(" TAnggal Lahir : " + tanggalLahir );
+    System.out.println(" Alamat        : " + alamat);
+    System.out.println(" Jurusan       : " + jurusan);
+    System.out.println(" Prodi         : " + prodi);
+    System.out.println(" Semester      : " + semester);
+    System.out.println(" Kelas         : " + kelas);
+   }
+}
